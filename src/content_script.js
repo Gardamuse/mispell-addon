d0dbbffc63128c07727486ffac73376f1e76fa48
gardamuse <gardamuse@gmail.com>
2021-07-29 14:38:32 +0200

    Now only modifying certain tags.

--- src/content_script.js
+++ src/content_script.js
@@ -3,18 +3,28 @@ import mispell from "mispell/dist/mispell.node";
 console.log("Content...4")
 //console.log("Print", mispell.bimbofy("Hello there my friends", 1.0))
 
-let elements = document.getElementsByTagName('*')
+function getElements(tagNames) {
+    let elements = []
+    for (let tag of tagNames) {
+        elements.push(...document.getElementsByTagName(tag))
+    }
+    return elements
+}
+
+let elements = getElements(["p", "h1", "h2", "h3", "h4", "h5", "h6", "span"])
 
 for (let element of elements) {
     for (let node of element.childNodes) {
         if (node.nodeType === 3) {
-            let text = node.nodeValue
-            //if (text.length < 10) continue
+            let text = node.textContent
 
-            let replacedText = mispell.bimbofy(text, 0.7)
+            //console.log(`Text: "${text}"`)
+            let replacedText = mispell.bimbofy(text, 0.8)
+            //let replacedText = text
             console.log(`Text: "${text}"\n\n Replaced with: "${replacedText}"`)
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node)
+                //node.innerText = replacedText
             }
         }
     }
