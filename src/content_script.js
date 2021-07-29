dba491432136ea97a475bbec3aeb60039c5b660c
gardamuse <gardamuse@gmail.com>
2021-07-29 18:51:31 +0200

    Now seems to work on both static and dynamic content.

--- src/content_script.js
+++ src/content_script.js
@@ -1,8 +1,11 @@
 import mispell from "mispell/dist/mispell.node";
 
-console.log("Content...4")
-//console.log("Print", mispell.bimbofy("Hello there my friends", 1.0))
+function replaceText(text) {
+    // TODO Get replacement function and amount from extension settings
+    return mispell.bimbofy(text, 0.8)
+}
 
+// Replace static content
 function getElements(tagNames) {
     let elements = []
     for (let tag of tagNames) {
@@ -11,21 +14,32 @@ function getElements(tagNames) {
     return elements
 }
 
-let elements = getElements(["p", "h1", "h2", "h3", "h4", "h5", "h6", "span"])
-
+let elements = getElements(["p", "h1", "h2", "h3", "h4", "h5", "h6", "span", "div"])
 for (let element of elements) {
     for (let node of element.childNodes) {
         if (node.nodeType === 3) {
+            let element = node.parentElement
             let text = node.textContent
-
-            //console.log(`Text: "${text}"`)
-            let replacedText = mispell.bimbofy(text, 0.8)
-            //let replacedText = text
-            console.log(`Text: "${text}"\n\n Replaced with: "${replacedText}"`)
+            let replacedText = replaceText(text)
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node)
-                //node.innerText = replacedText
             }
         }
     }
 }
+
+// Listen for and replace dynamically loaded content
+new MutationObserver(function(mutations) {
+    mutations.forEach(mutation => {
+        if(mutation.type === 'childList'){
+            mutation.addedNodes.forEach(addedNode => {
+                let treeWalker = document.createTreeWalker(addedNode, NodeFilter.SHOW_TEXT)
+                let textNode
+                while (textNode = treeWalker.nextNode()) {
+                    let text = textNode.nodeValue
+                    textNode.nodeValue = replaceText(text)
+                }
+            });
+        }
+    })
+}).observe(document, {childList: true, subtree: true})
