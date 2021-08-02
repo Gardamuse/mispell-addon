dcd592f552f97e1fee346dd174efe9a5b9f6d65a
gardamuse <gardamuse@gmail.com>
2021-08-02 19:01:41 +0200

    Browser Action popup is now visible. Trying to add settings storage.

--- src/background_script.js
+++ src/background_script.js
@@ -1,2 +1,8 @@
 // Put all the javascript code here, that you want to execute in background.
 console.log("Background script enabled")
+
+function handleClick() {
+    browser.runtime.openOptionsPage();
+}
+
+browser.browserAction.onClicked.addListener(handleClick);
--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,15 @@ const WebExtPlugin = require('web-ext-plugin');
 
 module.exports = {
     mode: 'production',
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
     entry: {
         background_script: './src/background_script.js',
-        content_script: './src/content_script.js'
+        content_script: './src/content_script.js',
+        options_script: './src/options/script.js',
+        browser_action_script: './src/browserAction/script.js'
     },
     module: {
         rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }],
@@ -18,7 +24,8 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-        //new HtmlWebpackPlugin({ template: 'src/popup/index.html' }),
+        new HtmlWebpackPlugin({ template: 'src/options/index.html', filename: "options.html"}),
+        new HtmlWebpackPlugin({ template: 'src/browserAction/index.html', filename: "browserAction.html"}),
         new CopyWebpackPlugin({
             patterns: [
                 { from: './src/manifest.json' },
