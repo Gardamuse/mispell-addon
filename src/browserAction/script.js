af1cf3bd89d525ef6853dd92782cc792525ddea3
gardamuse <gardamuse@gmail.com>
2021-08-08 01:07:17 +0200

    Can now use local storage.

--- src/browserAction/script.js
+++ src/browserAction/script.js
@@ -1,18 +1,21 @@
 document.getElementById('myHeading').style.color = 'blue'
 function saveOptions(e) {
-    browser.storage.sync.set({
+    console.log("Saving options")
+    browser.storage.local.set({
         colour: document.querySelector("#colour").value
     });
     e.preventDefault();
+    restoreOptions()
 }
 
 function restoreOptions() {
-    var storageItem = browser.storage.managed.get('colour');
+    console.log("Restoring options")
+    var storageItem = browser.storage.local.get('colour');
     storageItem.then((res) => {
         document.querySelector("#managed-colour").innerText = res.colour;
     });
 
-    var gettingItem = browser.storage.sync.get('colour');
+    var gettingItem = browser.storage.local.get('colour');
     gettingItem.then((res) => {
         document.querySelector("#colour").value = res.colour || 'Firefox red';
     });
